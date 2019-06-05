ed8b6fb5ab2aa32b2afa8bcf7dfcd0e1a6cbfb55
Akash.Sharma <akashanjana08@gmail.com>
2019-06-05 17:43:47 +0530

    TestLib as dependency

---
+++ logutill/src/main/java/io/vidyo/rsi/com/logutill/LogUtill.java
@@ -0,0 +1,12 @@
+package io.vidyo.rsi.com.logutill;
+
+import android.util.Log;
+
+public class LogUtill {
+
+    private static String TAG = "LogUtill";
+
+    public static void logUtill(String value){
+        Log.d(TAG, value);
+    }
+}
